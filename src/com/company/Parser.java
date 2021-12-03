2253d69fb01dcb8fae6fac830d58d286e12b79b0
SuJingyu111 <gar123su@gmail.com>
2021-12-03 00:32:41 -0500

    Parser parse account info method

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -20,4 +20,26 @@ public class Parser {
         return foreignExchange;
 
     }
+
+    //Acount record layout: id, name, pwd, accoutType, balance
+    public List<String[]> parseAccount(String name, String pwd, String filePath) {
+        String delimiter = ",";
+        String record = null;
+        List<String[]> accountInfoList = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            while ((record = br.readLine()) != null) {
+                String[] accountInfo = record.split(delimiter);
+                if (accountInfo[1].equals(name) && accountInfo[2].equals(pwd)) {
+                    accountInfoList.add(accountInfo);
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return accountInfoList;
+    }
+
 }
