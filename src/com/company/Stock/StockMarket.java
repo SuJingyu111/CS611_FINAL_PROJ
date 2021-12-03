d154ec679449e9076d7bc1e6950062e5cba5da09
SuJingyu111 <gar123su@gmail.com>
2021-12-03 13:47:08 -0500

    Singleton StockMarket

--- src/com/company/Stock/StockMarket.java
+++ src/com/company/Stock/StockMarket.java
@@ -1,5 +1,6 @@
 package com.company.Stock;
 
+import com.company.Exceptions.StockNotExistException;
 import com.company.Factories.StockFactory;
 
 import java.util.Map;
@@ -10,12 +11,21 @@ public class StockMarket {
 
     private StockFactory stockFactory;
 
-    public StockMarket() {
+    private static StockMarket stockMarket = new StockMarket();
+
+    private StockMarket() {
         stockFactory = new StockFactory();
         stockInfo = stockFactory.getAllStocks();
     }
 
-    public Stock getStockByName(String name) {
+    public static StockMarket getInstance() {
+        return stockMarket;
+    }
+
+    public Stock getStockByName(String name) throws StockNotExistException {
+        if (!stockInfo.containsKey(name)) {
+            throw new StockNotExistException();
+        }
         return stockInfo.get(name);
     }
 }
