3127af886ea52fb425370bc1b633f907feec5311
SuJingyu111 <gar123su@gmail.com>
2021-12-03 10:16:06 -0500

    write account

--- src/com/company/Writer.java
+++ src/com/company/Writer.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import ATM.Account.Account;
+
 import java.util.*;
 import java.io.*;
 
 public class Writer {
+
+    private final String CUST_PATH = "DbFiles/Customers.csv";
+
+    private final String MANAGER_PATH = "DbFiles/Manager.csv";
+
+    private final String CUST_ACC_PATH = "DbFiles/CustomerAccounts.csv";
+
+    private final String MANAGER_ACC_PATH = "DbFiles/ManagerAccounts.csv";
 
     public void writeForex(HashMap<String, Double> foreignExchange) throws IOException {
 
@@ -21,4 +31,16 @@ public class Writer {
         writer.write(String.valueOf(stringBuilder));
 
     }
+
+    public void writeAccount(Account account) throws IOException {
+        FileWriter writer = new FileWriter(CUST_ACC_PATH, false);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(account.getAccountId()).append(",");
+        stringBuilder.append(account.getOwnerName()).append(",");
+        stringBuilder.append(account.getPwd()).append(",");
+        stringBuilder.append(account.getTYPE()).append(",");
+        stringBuilder.append(account.getBalance());
+        writer.write(stringBuilder.toString());
+    }
+
 }
