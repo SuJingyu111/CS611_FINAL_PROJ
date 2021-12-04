3b0544a5782175cb84363c6f716ced4d3fbe39d2
SuJingyu111 <gar123su@gmail.com>
2021-12-04 18:53:36 -0500

    Trivial

--- src/com/company/Account/SavingsAccount.java
+++ src/com/company/Account/SavingsAccount.java
@@ -1,7 +1,6 @@
 package com.company.Account;
 
 public class SavingsAccount extends Account{
-    //TODO
 
     public SavingsAccount(String accountId, String ownerName, String pwd, AccountType type, int balance) {
         super(accountId, ownerName, pwd, type, balance);
