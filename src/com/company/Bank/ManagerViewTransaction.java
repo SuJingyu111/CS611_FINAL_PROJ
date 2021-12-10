e6f27dd32437e4b5452e0a6141b84b3e820ca5c8
BALAJI UDAYAKUMAR <balaji@bu.edu>
2021-12-10 16:06:12 -0500

    ManagerViewTransaction

--- src/com/company/Bank/ManagerViewTransaction.java
+++ src/com/company/Bank/ManagerViewTransaction.java
@@ -1,8 +1,12 @@
 package com.company.Bank;
 
 import com.company.Persons.Manager;
+import com.company.Transactions.Transaction;
+import com.company.Utils.Parser;
+import com.company.Utils.Writer;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.*;
 
 public class ManagerViewTransaction {
@@ -10,12 +14,22 @@ public class ManagerViewTransaction {
     public static void run(Manager manager, Currency currency) throws IOException {
 
         Scanner input = new Scanner(System.in);
+        Parser parser = new Parser();
 
         System.out.println();
         System.out.println("Enter date : ( YYYY/MM/DD ) ");
-        String date = input.next();
+        String date1 = input.next();
+        LocalDate date = LocalDate.parse(date1);
+        List<Transaction> transactions = parser.parseTxnByDate(date);
+        int count = 1;
 
-
+        System.out.println();
+        System.out.println("********************************************************************************************");
+        System.out.println("        Transaction ID      Date       Amount      Customer ID      Type ");
+        for (Transaction transaction : transactions){
+            System.out.println("<" + count + ">" + "    " + transaction.getId() + "   " + transaction.getTimestamp() + "    " + transaction.getAmount() + "    " + transaction.getPersonId() + "    " + transaction.getType());
+            count += 1;
+        }
 
         ManagerOptions.options(manager, currency);
 
