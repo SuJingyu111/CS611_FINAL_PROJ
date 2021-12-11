de95da127fc03f7e197a158203b81c92194c2b87
SuJingyu111 <gar123su@gmail.com>
2021-12-11 17:13:10 -0500

    Javadoc comment for Stock/StockMarket

--- src/com/company/Stock/StockMarket.java
+++ src/com/company/Stock/StockMarket.java
@@ -5,12 +5,24 @@ import com.company.Factories.StockFactory;
 
 import java.util.Map;
 
+/**
+ * Class for stock Market. It is a singleton.
+ */
 public class StockMarket {
 
+    /**
+     * Information of all stocks available.
+     */
     private Map<String, Stock> stockInfo;
 
+    /**
+     * StockFactory {@link com.company.Factories.StockFactory}.
+     */
     private StockFactory stockFactory;
 
+    /**
+     * Singleton object.
+     */
     private static StockMarket stockMarket = new StockMarket();
 
     private StockMarket() {
@@ -18,10 +30,22 @@ public class StockMarket {
         stockInfo = stockFactory.getAllStocks();
     }
 
+    /**
+     * Get instance of StockMarket.
+     *
+     * @return Instance of StockMarket
+     */
     public static StockMarket getInstance() {
         return stockMarket;
     }
 
+    /**
+     * Get stock by its name.
+     *
+     * @param name Name of stock
+     * @return Instance of that stock
+     * @throws StockNotExistException If the stock does not exist
+     */
     public Stock getStockByName(String name) throws StockNotExistException {
         if (!stockInfo.containsKey(name)) {
             throw new StockNotExistException();
@@ -29,6 +53,9 @@ public class StockMarket {
         return stockInfo.get(name);
     }
 
+    /**
+     * Refreshes the market if there are updates.
+     */
     public void refresh() {
         stockInfo = stockFactory.getAllStocks();
     }
