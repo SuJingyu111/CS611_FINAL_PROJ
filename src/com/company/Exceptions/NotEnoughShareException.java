d38ff15a3a3eb852280cc58cb80b3cac8c85f11e
SuJingyu111 <gar123su@gmail.com>
2021-12-11 15:00:34 -0500

    Javadoc comments for exceptions

--- src/com/company/Exceptions/NotEnoughShareException.java
+++ src/com/company/Exceptions/NotEnoughShareException.java
@@ -1,5 +1,8 @@
 package com.company.Exceptions;
 
+/**
+ * Exception to throw when the share is not enough
+ */
 public class NotEnoughShareException extends RuntimeException{
 
     public NotEnoughShareException() {super("Does not have enough share!");}
--- src/com/company/Exceptions/StockNotExistException.java
+++ src/com/company/Exceptions/StockNotExistException.java
@@ -1,5 +1,8 @@
 package com.company.Exceptions;
 
+/**
+ * Exception to throw when the stock does not exist
+ */
 public class StockNotExistException extends RuntimeException{
 
     public StockNotExistException() {
