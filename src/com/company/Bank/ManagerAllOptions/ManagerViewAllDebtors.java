e2a0777dfd71c540264ff0dcf79bbd5111270d7d
BALAJI UDAYAKUMAR <balaji@bu.edu>
2021-12-16 17:02:41 -0500

    ManagerViewAllDebtors

--- src/com/company/Bank/ManagerAllOptions/ManagerViewAllDebtors.java
+++ src/com/company/Bank/ManagerAllOptions/ManagerViewAllDebtors.java
@@ -1,10 +1,13 @@
 package com.company.Bank.ManagerAllOptions;
 
 import com.company.Currency.Currency;
+import com.company.Factories.PersonFactory;
+import com.company.Persons.Customer;
 import com.company.Persons.Manager;
 import com.company.Stock.StockMarket;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Class responsible for allowing managers to view all debtors
@@ -20,7 +23,8 @@ public class ManagerViewAllDebtors {
      */
     public static void run(Manager manager, Currency currency, StockMarket stockMarket) throws IOException{
 
-        
+        PersonFactory personFactory = new PersonFactory();
+        List<Customer> debtors = personFactory.
 
         ManagerOptions.options(manager, currency, stockMarket);
 
